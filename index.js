dd1b4eb8c9e10dc0ca4d2121e911a83029c5a88d
deepakvenkat <dipak2911@gmail.com>
2013-08-14 11:33:40 -0400

    setup basic socket io

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var express = require("express");
 var http = require("http");
 var _ = require("underscore");
+var socketio = require("socket.io");
+
 var app = express();
 var port = 3700;
 
 app.set('views', __dirname + '/templates');
 app.set('view engine', "jade");
 app.engine("jade", require("jade").__express);
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+
 var httpServer = http.createServer(app).listen(port, function () {
   console.log("Listening on port" + port);
 });
 
+var io = socketio.listen(httpServer);
+io.sockets.on("connection", function (socket) {
+  console.log("socket established connections");
+  socket.emit("message", {message: "welcome to the game"});
+});
 /*  ROUTES */
 
 app.get('/', function (req, res) {
-  res.render("game")
+  res.render("game");
 });
---
+++ public/js/game.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+
+  var socket = io.connect("/");
+
+  socket.on("message", function (data) {
+    console.log(data.message);
+  });
+}
